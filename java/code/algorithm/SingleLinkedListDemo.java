e33a2dfcc46e19caa7e354f53e1527161e46216f
黎义 <liyi@forchange.tech>
2019-08-01 01:12:07 +0800

    feat: sortedAdd&update with singlelinkedlist

--- java/code/algorithm/SingleLinkedListDemo.java
+++ java/code/algorithm/SingleLinkedListDemo.java
@@ -13,15 +13,31 @@ public class SingleLinkedListDemo{
         list.add(hero3);
         list.add(hero4);
         list.show();
-        
+        // 测试有序插入链表
+        HeroNode hero6 = new HeroNode(6, "鲁智深", "花和尚");
+        HeroNode hero5 = new HeroNode(5, "武松", "行者");
+        System.out.println("——————————————————————————————乱序插入后——————————————————");
+        list.sortedAdd(hero6);
+        list.sortedAdd(hero5);
+        list.show();
+
+        // 测试更新节点
+        HeroNode newHero4 = new HeroNode(6, "公孙胜", "入云龙");
+        list.update(newHero4);
+        System.out.println("——————————————————————————————更新节点后——————————————————————————");
+        list.show();
     }
 }
 
 class SingleLinkedList{
-    public HeroNode head = new HeroNode(0, "", "");
+    private HeroNode head = new HeroNode(0, "", "");
+
+    public HeroNode getHead(){
+        return this.head;
+    }
 
     public void add(HeroNode node){
-        HeroNode tmp = head;
+        HeroNode tmp = this.head;
         // 遍历链表
         while(tmp.next != null){
             // 指针往后移动一个节点
@@ -30,6 +46,46 @@ class SingleLinkedList{
         // 退出循环时， tmp指向最后一个节点
         tmp.next = node ;
     }
+
+    public void sortedAdd(HeroNode node){
+        /**
+         *   按照英雄no从小到大顺序插入
+         */
+        HeroNode tmp = this.head;
+        while (true){
+            if (tmp.next != null){
+                //no不允许重复，不用比较等于的情况
+                if (tmp.next.no > node.no){
+                    node.next = tmp.next;
+                    tmp.next = node;
+                    break;
+                }
+                tmp = tmp.next;
+            }else{
+                tmp.next = node;
+                node.next = null;
+                break;
+            }
+        }
+
+
+    }
+    public void update(HeroNode node){
+        /**
+         * 根据英雄编号更新  编号不存在给出错误提示
+         */
+        HeroNode tmp = this.head;
+        while(tmp.next != null){
+            if (tmp.next.no == node.no){
+                node.next = tmp.next.next;
+                tmp.next = node;
+                return ;
+            }
+            tmp = tmp.next;
+        }
+        System.out.println("未找到对应节点");
+    }
+
 
     public void show(){
         /**
